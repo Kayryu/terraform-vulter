2c9b5ae9d45aefb938f917c49fb147dbaabe4e18
kayryu <lukayryu@gmail.com>
2021-02-19 17:54:28 +0800

    fix output.tf

--- modules/vultr-vc/output.tf
+++ modules/vultr-vc/output.tf
@@ -5,5 +5,5 @@ output "instance_id" {
 
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
-  value       = vultr_instance.instance.main_id
+  value       = vultr_instance.instance.main_ip
 }
--- services/instance1/output.tf
+++ services/instance1/output.tf
@@ -1,9 +1,9 @@
 output "instance_id" {
   description = "ID of the instance"
-  value       = module.vultr-vc.instance_id
+  value       = module.server1.instance_id
 }
 
 output "instance_public_ip" {
   description = "Public IP address of the instance"
-  value       = module.vultr-vc.instance_public_ip
+  value       = module.server1.instance_public_ip
 }
